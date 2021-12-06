269f698acf3bb1093493b4ae738548b32982db30
IamOppy <randolffretrato@yahoo.com>
2021-12-05 17:20:03 -0800

    New Project Change from form to portfolio

--- WebForm.py
+++ WebForm.py
@@ -2,9 +2,21 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def default_home():
+     return render_template('home.html')
+     
+@app.route('/home')
 def home():
      return render_template('home.html')
+
+@app.route("/projects")
+def projects():
+     return render_template('projects.html')
+
+@app.route("/contact")
+def contact():
+     return render_template('contacts.html')
 
 if __name__=="__main__":
      app.run(debug=True)
